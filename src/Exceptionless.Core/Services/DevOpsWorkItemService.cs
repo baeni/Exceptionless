9461f5cc5b0d234acc9a6ba59aaddf93c2971265
Benjamin Saalfeld <benjamin.saalfeld@adesso.de>
2024-10-02 09:51:03 +0200

    Create DevOpsWOrkItemState enum (temporarily in this file) Split "UpdateWOrkItemStateAsync" into "UpdateRemoveWorkItemStateIfLinked" and "UpdateLocalWorkItemState" Restructure and fix logic

--- src/Exceptionless.Core/Services/DevOpsWorkItemService.cs
+++ src/Exceptionless.Core/Services/DevOpsWorkItemService.cs
@@ -4,6 +4,9 @@ using System.Text;
 using Microsoft.Extensions.Logging;
 using Exceptionless.Core.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Exceptionless.Core.Extensions;
+
+using Exceptionless.Core.Models;
 
 namespace Exceptionless.Core.Services;
 
@@ -22,52 +25,124 @@ public class DevOpsWorkItemService : IDevOpsWorkItemService
         _logger = loggerFactory.CreateLogger<DevOpsWorkItemService>();
     }
 
-    public async Task<IActionResult> LinkWorkItemToStackAsync(string stackId, string workItemId)
+    public async Task<IActionResult> LinkWorkItemToStack(string stackId, string workItemId, TimeProvider timeProvider)
     {
         var stack = await _stackRepository.GetByIdAsync(stackId);
         if (stack is null)
-            return new NotFoundResult();
+            return new NotFoundObjectResult($"Stack with Id {stackId} could not be found.");
 
-        var workItemState = await GetWorkItemStateAsync(workItemId);
+        var workItemState = await FetchWorkItemState(workItemId);
         if (workItemState is null)
-            return new BadRequestResult();
+            return new NotFoundObjectResult($"Work Item with Id {workItemId} could not be found in Azure DevOps.");
+
+        switch (workItemState)
+        {
+            case DevOpsWorkItemState.ToDo:
+                stack.MarkOpen();
+                break;
+            case DevOpsWorkItemState.Doing:
+                stack.MarkOpen();
+                break;
+            case DevOpsWorkItemState.Done:
+                stack.MarkFixed(null, timeProvider);
+                break;
+        }
 
         stack.DevOpsWorkItemId = workItemId;
-        stack.DevOpsWorkItemStateName = workItemState;
-
-        await _stackRepository.SaveAsync(stack);
-
-        return new OkResult();
-    }
-
-    public async Task<IActionResult> UnlinkWorkItemFromStackAsync(string stackId)
-    {
-        var stack = await _stackRepository.GetByIdAsync(stackId);
-        if (stack is null)
-            return new NotFoundResult();
-
-        stack.DevOpsWorkItemId = null;
-        stack.DevOpsWorkItemStateName = null;
+        stack.DevOpsWorkItemState = workItemState;
 
         await _stackRepository.SaveAsync(stack);
 
         return new NoContentResult();
     }
 
-    public async Task<IActionResult> UpdateWorkItemStateAsync(string workItemId, string newState)
+    public async Task<IActionResult> UnlinkWorkItemFromStack(string stackId)
     {
-        var stack = await _stackRepository.GetStackByDevOpsWorkItemIdAsync(workItemId);
+        var stack = await _stackRepository.GetByIdAsync(stackId);
         if (stack is null)
-            return new OkObjectResult("No Stacks affected.");
+            return new NotFoundObjectResult($"Stack with Id {stackId} could not be found.");
 
-        stack.DevOpsWorkItemStateName = newState;
+        stack.DevOpsWorkItemId = null;
+        stack.DevOpsWorkItemState = null;
 
         await _stackRepository.SaveAsync(stack);
 
-        return new OkResult();
+        return new NoContentResult();
     }
 
-    public async Task<string?> GetWorkItemStateAsync(string workItemId)
+    public async Task<IActionResult> UpdateRemoteWorkItemStateIfLinked(string stackId, StackStatus newStatus)
+    {
+        var stack = await _stackRepository.GetByIdAsync(stackId);
+        if (string.IsNullOrWhiteSpace(stack.DevOpsWorkItemId))
+            return new BadRequestObjectResult($"Stack with Id {stackId} is not linked to an Azure DevOps Work Item.");
+
+        var workItemId = stack.DevOpsWorkItemId;
+        DevOpsWorkItemState newWorkItemState;
+
+        switch (newStatus)
+        {
+            case StackStatus.Open:
+                newWorkItemState = DevOpsWorkItemState.ToDo;
+                break;
+            case StackStatus.Snoozed:
+                newWorkItemState = DevOpsWorkItemState.ToDo;
+                break;
+            case StackStatus.Fixed:
+            case StackStatus.Ignored:
+            case StackStatus.Discarded:
+                newWorkItemState = DevOpsWorkItemState.Done;
+                break;
+            default:
+                newWorkItemState = DevOpsWorkItemState.ToDo;
+                break;
+        }
+
+        var httpClient = _httpClientFactory.CreateClient("devops");
+
+        var encodedPat = Convert.ToBase64String(Encoding.ASCII.GetBytes($":{_pat}"));
+        var request = new HttpRequestMessage(HttpMethod.Patch, $"WorkItems/{workItemId}?api-version=7.1");
+        request.Headers.Authorization = new AuthenticationHeaderValue("Basic", encodedPat);
+        request.Content = new StringContent(
+            JsonSerializer.Serialize(
+                new[]
+                {
+                    new { op = "add", path = "/fields/System.State", value = newWorkItemState.ToDevOpsString() }
+                }),
+            Encoding.UTF8, "application/json-patch+json");
+
+        var response = await httpClient.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+
+        return new NoContentResult();
+    }
+
+    public async Task<IActionResult> UpdateLocalWorkItemState(string workItemId, DevOpsWorkItemState newWorkItemState, TimeProvider timeProvider)
+    {
+        var stack = await _stackRepository.GetStackByDevOpsWorkItemIdAsync(workItemId);
+        if (stack is null)
+            return new NoContentResult();
+
+        switch (newWorkItemState)
+        {
+            case DevOpsWorkItemState.ToDo:
+                stack.MarkOpen();
+                break;
+            case DevOpsWorkItemState.Doing:
+                stack.MarkOpen();
+                break;
+            case DevOpsWorkItemState.Done:
+                stack.MarkFixed(null, timeProvider);
+                break;
+        }
+
+        stack.DevOpsWorkItemState = newWorkItemState;
+
+        await _stackRepository.SaveAsync(stack);
+
+        return new NoContentResult();
+    }
+
+    private async Task<DevOpsWorkItemState?> FetchWorkItemState(string workItemId)
     {
         var httpClient = _httpClientFactory.CreateClient("devopsanalytics");
 
@@ -88,9 +163,13 @@ public class DevOpsWorkItemService : IDevOpsWorkItemService
             if (valueArray.GetArrayLength() > 0)
             {
                 var firstItem = valueArray[0];
-                var stateName = firstItem.GetProperty("State").GetString();
+                var workItemStateStr = firstItem.GetProperty("State").GetString();
 
-                return stateName;
+                if (!string.IsNullOrEmpty(workItemStateStr))
+                {
+                    var workItemState = DevOpsWorkItemStateExtensions.FromDevOpsString(workItemStateStr);
+                    return workItemState;
+                }
             }
         }
 
@@ -100,11 +179,18 @@ public class DevOpsWorkItemService : IDevOpsWorkItemService
 
 public interface IDevOpsWorkItemService
 {
-    Task<IActionResult> LinkWorkItemToStackAsync(string stackId, string workItemId);
+    Task<IActionResult> LinkWorkItemToStack(string stackId, string workItemId, TimeProvider timeProvider);
 
-    Task<IActionResult> UnlinkWorkItemFromStackAsync(string stackId);
+    Task<IActionResult> UnlinkWorkItemFromStack(string stackId);
 
-    Task<IActionResult> UpdateWorkItemStateAsync(string workItemId, string newState);
+    Task<IActionResult> UpdateRemoteWorkItemStateIfLinked(string stackId, StackStatus newStatus);
 
-    Task<string?> GetWorkItemStateAsync(string workItemId);
+    Task<IActionResult> UpdateLocalWorkItemState(string workItemId, DevOpsWorkItemState newWorkItemState, TimeProvider timeProvider);
+}
+
+public enum DevOpsWorkItemState
+{
+    ToDo,
+    Doing,
+    Done
 }
