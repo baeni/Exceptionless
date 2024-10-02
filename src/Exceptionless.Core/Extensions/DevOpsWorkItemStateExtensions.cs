e490ad4e62a0953e31c062bc1635275b3a44a294
Benjamin Saalfeld <benjamin.saalfeld@adesso.de>
2024-10-02 09:49:50 +0200

    Create DevOpsWorkItemStateExtension to get DevOpsWorkItemState by string and vice versa

---
+++ src/Exceptionless.Core/Extensions/DevOpsWorkItemStateExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Exceptionless.Core.Services;
+
+namespace Exceptionless.Core.Extensions;
+
+public static class DevOpsWorkItemStateExtensions
+{
+    public static string ToDevOpsString(this DevOpsWorkItemState workItemState)
+    {
+        return workItemState switch
+        {
+            DevOpsWorkItemState.ToDo => "To Do",
+            DevOpsWorkItemState.Doing => "Doing",
+            DevOpsWorkItemState.Done => "Done",
+            _ => throw new ArgumentOutOfRangeException(nameof(workItemState), workItemState, null)
+        };
+    }
+
+    public static DevOpsWorkItemState FromDevOpsString(string workItemStateStr)
+    {
+        return workItemStateStr switch
+        {
+            "To Do" => DevOpsWorkItemState.ToDo,
+            "Doing" => DevOpsWorkItemState.Doing,
+            "Done" => DevOpsWorkItemState.Done,
+            _ => throw new ArgumentException($"Unknown work item state: {workItemStateStr}", nameof(workItemStateStr))
+        };
+    }
+}
